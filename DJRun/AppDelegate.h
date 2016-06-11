ed260972917791fa03ce2d7ce277f1d30d70cb7e
dujia <815319775@qq.com>
2016-06-11 13:11:11 +0800

    Initial Commit

---
+++ DJRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJRun
+//
+//  Created by dujia on 16/6/11.
+//  Copyright © 2016年 dujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJRun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJRun
+//
+//  Created by dujia on 16/6/11.
+//  Copyright © 2016年 dujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
